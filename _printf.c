2555b0c6454eb847f8d6f4644c1b3c3011e1eeef
Sundamo <sundamo1993@gmail.com>
2023-09-17 10:40:09 -0700

    kk

--- _printf.c
+++ _printf.c
@@ -9,39 +9,38 @@
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
+
 	va_list list_of_args;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start (list_of_args, format);
+	va_start(list_of_args, format);
 
-	while (*format) // a loop that interates through characters of the format
+	while (*format)
 	{
-		if (*format != '%') //if format is not the % sign
+		if (*format != '%')
 		{
-			write (1, format, 1);
+			write(1, format, 1);
 			chara_print++;
 		}
 
-		else // if format is the % sign 
+		else
 		{
 			format++;
 			if (*format == '\0')
 				break;
 
-			if (*format == '%') // this solves ('%')
+			if (*format == '%')
 			{
-				//handle '%%' (double '%')
-				write (1, format, 1);
+				write(1, format, 1);
 				chara_print++;
 			}
 
 			else if (*format == 'c')
 			{
-				//handle '%c' (character)
 				char c = va_arg(list_of_args, int);
-				write (1, &c, 1);
+				write(1, &c, 1);
 				chara_print++;
 			}
 
@@ -50,12 +49,10 @@ int _printf(const char *format, ...)
 				char *str = va_arg(list_of_args, chars*);
 				int str_len = 0;
 
-				// calculate the lenght of the string
 				while (str[str_len] != '\0')
 					str_len++;
 
-				//write this string to the standard output
-				write (1, str, str_len);
+				write(1, str, str_len);
 				chara_print += str_len;
 			}
 		}
@@ -63,7 +60,7 @@ int _printf(const char *format, ...)
 		format++
 	}
 
-	va_end (list_of_args);
+	va_end(list_of_args);
 
-	return chara_print;
+	return (chara_print);
 }
